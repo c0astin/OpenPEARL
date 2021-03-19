6885b5bf85fa23d97456d92a0d2270745cb64370
mueller.debian9 <mueller@hs-furtwangen.de>
2021-03-19 15:21:24 +0100

    sleep added to finish write of .exp-file

--- openpearl-code/errorChecker/runTest.sh
+++ openpearl-code/errorChecker/runTest.sh
@@ -12,6 +12,7 @@ do
    basename=${filename%.*}
    $path/genExpectations $filename #1>/dev/null
    rc=$?
+   sleep 1
    if [ $rc -ne 0 ] ; then
       printf "%-40s :  problems in annotation definition\n" $1
       rm -f $basename.exp 
