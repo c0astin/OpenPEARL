e2644c9bd653d98200fd9b39685ee5bf2fc33037
mueller.debian9 <mueller@hs-furtwangen.de>
2021-04-20 12:58:23 +0200

    missing file added

---
+++ openpearl-code/errorChecker/config.h
@@ -0,0 +1,4 @@
+// some settings for errorChecker und genExpectations
+#define LINELENGTH  1024  // max length of an error message
+#define SOURCELEN   250   // max. length of a file name in an error message
+
