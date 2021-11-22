f65090ac82783574d06f9168e1d05e708dac4f09
Rainer Müller@deb10 <mueller@hs-furtwangen.de>
2021-11-22 14:58:27 +0100

    test for ticket 344 added - works fine CommonErrorMessages added missing closing brackets in messages

--- openpearl-code/compiler/src/org/smallpearl/compiler/CommonErrorMessages.java
+++ openpearl-code/compiler/src/org/smallpearl/compiler/CommonErrorMessages.java
@@ -31,16 +31,16 @@ public class CommonErrorMessages {
 
     public static void wrongFloatPrecission(ParserRuleContext ctx) {
         ErrorStack.add(ctx, "illegal precision", "not in set [" + Defaults.FLOAT_SHORT_PRECISION
-                + "," + Defaults.FLOAT_LONG_PRECISION);
+                + "," + Defaults.FLOAT_LONG_PRECISION + "]");
     }
 
     public static void wrongBitLength(ParserRuleContext ctx) {
         ErrorStack.add(ctx, "illegal length",
-                "not in range [" + Defaults.BIT_MIN_LENGTH + "," + Defaults.BIT_MAX_LENGTH);
+                "not in range [" + Defaults.BIT_MIN_LENGTH + "," + Defaults.BIT_MAX_LENGTH + "]");
     }
 
     public static void wrongCharLength(ParserRuleContext ctx) {
         ErrorStack.add(ctx, "illegal length",
-                "not in range [" + Defaults.CHARACTER_LENGTH + "," + Defaults.CHARACTER_MAX_LENGTH);
+                "not in range [" + Defaults.CHARACTER_LENGTH + "," + Defaults.CHARACTER_MAX_LENGTH + "]");
     }
 }
